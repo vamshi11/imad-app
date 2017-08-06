77d3dbde9e1c5a6fd01d7866ba5b65ac75f1c5e6
vamshi11 <30730458+vamshi11@users.noreply.github.com>
2017-08-06 10:35:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,6 +21,10 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+ app.get('/main.js', function (req, res){
+    res.sendFile(path.join(__dirnme, 'ui', 'main.js'));
+});
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
