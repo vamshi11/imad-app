ad30039ae0ad17c3de305a50e49eaae41d90276e
vamshi11 <30730458+vamshi11@users.noreply.github.com>
2017-08-06 12:59:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,5 @@
 console.log('Loaded!');
 
-var element = document.getElementById('main-text'
-);
-
-element.innerHTML = "New value";
 
 var img = document.getElementById('madi');
 img.onclick = function(){
