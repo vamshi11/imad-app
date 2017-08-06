e52d6be44e5b5dff3ca77c8f01000993679d675d
vamshi11 <30730458+vamshi11@users.noreply.github.com>
2017-08-06 13:39:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,9 +14,12 @@ app.get('/article-one', function (req, res){
     app.get('/article-two', function (req, res){
     res.send('sjdfhsjdf article-two');
 });
-    app.get('/article-three', function (req, res){
-    res.send('sjdfhsjdf article-three');
+    var counter=0;
+    app.get('/counter', function (req, res){
+        counter=counter+1;
+        res.send(counter.toString());
 });
+    
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
