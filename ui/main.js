3de597db39be67dd37c38000c126cfd58dca4593
vamshi11 <30730458+vamshi11@users.noreply.github.com>
2017-08-06 13:09:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,11 @@ console.log('Loaded!');
 
 
 var img = document.getElementById('madi');
+var marginLeft=0;
+function moveRight(){
+    marginLeft = marginLeft+10;
+    img.style.matginLeft = marginLeft+'px';
+}
 img.onclick = function(){
-    img.style.marginleft ='100px';
+    var interval = setInterval(moveRight,100)
 };
