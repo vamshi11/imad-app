730fcd717cd468baaf8ec52bdd98191e35c39cf2
vamshi11 <30730458+vamshi11@users.noreply.github.com>
2017-08-06 12:53:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,8 @@ console.log('Loaded!');
 var element = document.getElemenById('main-text');
 
 element.innerHTML = 'New value';
+
+var img = document.getElementById("img");
+img.onclick = function(){
+    img.style.moveleft='100px';
+}
